64c1aa00dc7dfac2d719d74c6bbe563864ddab58
GK <32246900+gkproctor@users.noreply.github.com>
2025-08-30 22:46:40 -0700

    adding favicon to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
         <header className="max-w-screen-sm mx-auto px-3 py-3 flex items-center justify-between">
           <Link href="/" className="text-lg font-bold">VanMaps</Link>
